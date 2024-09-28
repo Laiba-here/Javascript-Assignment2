6394e814747b44baa969743c857793598a2965d7
Laiba-here <nazlaiba230@gmail.com>
2024-09-27 21:50:48 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+ //          .... QUESTION 1 ....
+//let username;
+
+       // username = "Laiba";
+
+        // alert("Welcome, " + username + "!");
+
+//           .... QUESTION 2 ....
+// var myName = "Laiba Naz"
+
+// alert("Hello, my name is " + myName + "!");
+
+//       .... QUESTION 3 ....
+// var message;
+
+// message = "Hello World";
+
+// alert(message);
+       
+//         .... QUESTION 4 ....
+// var studentName = "Laiba Naz";
+// var studentAge = 21;
+// var studentCertification = "Certified Mobile App Development";
+
+// var message = "Name: " + studentName + "\n" +
+//               "Age: " + studentAge + " Years Old\n" +
+//               "Certification: " + studentCertification;
+
+// alert(message);
+
+//      .... QUESTION 5 ....
+
+// var pizzaMessage = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizzaMessage);
+
+//       .... QUESTION 6 ....
+// var email = "example@example.com";
+// var message = "My Email Address is " + email;
+// alert(message);
+
+//      .... question 7 ....
+
+// var book = "A Smarter way to learn JavaScript";
+// alert("I am learning from the book: " + book);
+
+//         .... QUESTION 8 ....
+// document.write("Yah! I can write HTML content through JavaScript");
+
+//           .... QUESTION 9 ....
+var myString = "------<>------";
+    alert(myString);
+    document.write(myString);
